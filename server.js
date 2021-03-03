d8b55a0742040de4669499012eb121599cfde855
danielbednarz2 <danielbednarz2@gmail.com>
2021-03-03 15:50:42 -0800

    Initial commit setting up server

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import nodemailer from 'nodemailer'
+import cors from 'cors'
+
+const app = express();
+const PORT = process.env.PORT;
+const GMAIL_USER = process.env.GMAIL_USER;
+const GMAIL_PW = process.env.GMAIL_PW;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.post('/submit-form',  (req, res) => {
+
+    const { email, firstName, message } = req.body;
+    
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: GMAIL_USER,
+            pass: GMAIL_PW
+        }
+    })
+
+    const info =  transporter.sendMail({
+        from: `${email}`,
+        to: GMAIL_USER,
+        subject: 'Portfolio Contact Form',
+        text: `${firstName} says ${message}`
+    })
+})
+
+app.listen(PORT)
